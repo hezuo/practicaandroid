bb00b74d0cfe139453c718da3234084d9d721f46
Cesar Pillihuaman <cesarpillihuaman@gmail.com>
2014-11-27 00:56:41 -0500

    fix en configuracion manifest

--- src/pe/edu/upc/practica01/MainActivity.java
+++ src/pe/edu/upc/practica01/MainActivity.java
@@ -7,9 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import pe.edu.upc.practica01.application.MyApplication;
-
-
 public class MainActivity extends GeneralActivity {
 
     private Button btLogin;
